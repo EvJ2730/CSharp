3c55b8daab218c520d4ba79f2d520f643fe168c5
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-01 23:20:35 +0200

    exercise6

---
+++ homework/25.03.21/exercise1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an odd or even number");
+            string stringNumber1 = Console.ReadLine();
+            Console.WriteLine("Enter the opposite number from the first");
+            string stringNumber2 = Console.ReadLine();
+
+            int number1 = int.Parse(stringNumber1);
+            int number2 = int.Parse(stringNumber2);
+            
+            if (number1 % 2 == 0 && number2 % 2 == 0 ^ number2 % 2 == 0 || number1 % 2 == 0 )
+            {
+                Console.WriteLine("Number1 is even and Number2 is odd.");
+            } else
+            {
+                Console.WriteLine("Number1 is odd and Number2 is even.");
+            }
+        }
+    }
+}
