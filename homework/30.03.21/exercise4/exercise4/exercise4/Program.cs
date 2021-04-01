900edf3a390747f951e23dcec13ef0982199b5de
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-01 23:35:53 +0200

    homework

---
+++ homework/30.03.21/exercise4/exercise4/exercise4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = new string [] { "Nikola", "Boban", "Martin", "Stefan", "Zoran"};
+            double[] doubleArray = new double [] { 3.14, 18.25, 16.238, 30.12, 27.27};
+            char[] charArray = new char []{ 'h', 'M','w','l','P'};
+            bool[] boolArray = new bool []{ true, false, true, true, false} ;
+            int [][] jaggedArray = new int [][]
+            {
+                new int [] {2 , 5},
+                new int [] {8 , 6},
+                new int [] {28 , 45},
+                new int [] {36 , 135},
+                new int [] {258 , 15},
+             };
+        }
+    }
+}
