52b4fa2107ae3e20b3b74c386d99d26fa59e40ac
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-01 23:22:44 +0200

    Real Calculator

---
+++ homework/25.03.21/task1/RealCalculator/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter one number");
+            string stringNumber1 = Console.ReadLine();
+            int number1 = int.Parse(stringNumber1);
+
+            Console.WriteLine("Enter another number");
+            string stringNumber2 = Console.ReadLine();
+            int number2 = int.Parse(stringNumber2);
+
+            Console.WriteLine("Enter the wanted operator (+, -, /, *)");
+            string math = Console.ReadLine();
+            switch (math)
+            {
+                case "+":
+                    Console.WriteLine(number1 + number2) ;
+                    break;
+                case "-":
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case "*":
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case "/":
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    Console.WriteLine("You have entered a wrong operator");
+                    break;
+            }
+
+            //if (math == "+")
+            //{
+            //    Console.WriteLine(number1 + number2);
+            //}
+            //else if (math == "-")
+            //{
+            //    Console.WriteLine(number1 - number2);
+            //}
+            //else if (math == "*")
+            //{
+            //    Console.WriteLine(number1 * number2);
+            //}
+            //else if (math == "/")
+            //{
+            //    Console.WriteLine(number1 / number2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You've entered an invalid number!");
+            //}
+        }
+    }
+}
