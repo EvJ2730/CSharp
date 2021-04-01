4276de2fe442ac97eefff51608b6cf9d4f92f7ce
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-01 23:27:48 +0200

    Swap Numbers

---
+++ homework/25.03.21/task3/SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Enter another number");
+            string secondNumber = Console.ReadLine();
+
+            int number1 = int.Parse(firstNumber);
+            int number2 = int.Parse(secondNumber);
+
+            Console.WriteLine("Before swaping");
+            Console.WriteLine($"First number is : {number1}");
+            Console.WriteLine($"Second number is : {number2}");
+
+            int number = number1;
+            number1 = number2;
+            number2 = number;
+
+            Console.WriteLine("After swaping");
+            Console.WriteLine($"First number is now: {number1}");
+            Console.WriteLine($"Second number is now: {number2}");
+        }
+    }
+}
