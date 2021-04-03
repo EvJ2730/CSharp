5500ef15ad1416e7aef454515fb3c464c741c169
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-03 04:32:37 +0200

    unfinished

---
+++ homework/01.04.21/task1/funWithStrings/funWithStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace funWithStrings
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("Enter a sentence/string");
+            string stringInput = Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            
+
+        }
+        
+    }
+}
