cc35a43acfe57ddbe0c2d1c442f34ed0bd4b6277
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-08 17:37:35 +0200

    new homework

---
+++ homework/03.04.21/race/race/Driver/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace race.Driver
+{
+    class Driver
+    {
+        public string Name { get; set; }
+        public string Skill { get; set; }
+        
+    }
+}
---
+++ homework/03.04.21/race/race/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using race.Car;
+using race.Driver;
+
+namespace race
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        
+    }
+}
