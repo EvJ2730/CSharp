15144e7638f1c1e79134664777e4029fe30592ae
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-16 16:24:40 +0200

    x

---
+++ homework/03.04.21/race/race/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using race.Classes;
+namespace race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver max = new Driver("Max",2);
+            Driver ron = new Driver("Ron",5);
+            Driver lana = new Driver("Lana",10);
+            Driver rudi = new Driver("Rudi",8);
+
+            Car hyndai = new Car("Hyndai","Max",130 );
+            Car mazda = new Car("Mazda","Ron",170 );
+            Car ferrari = new Car("Ferrari","Lana",350 );
+            Car porsche = new Car("Porsche","Rudi",270 );
+        }
+    }
+}
---
+++ homework/08.04.21/task1/enums/enums/Models/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models.Enums;
+namespace enums.Models
+{
+    public class Manager : Employee 
+    {
+        public Manager(string firstName, string lastName, double salary) 
+            : base(firstName, lastName, salary, Role.Manager) { }
+        private double Bonus { get; set; }
+        public void AddBonus(double bonus)
+        {
+            Bonus += bonus;
+        }
+
+        public override double GetSalary()
+        {
+            return Salary + Bonus;
+        }
+    }
+}
