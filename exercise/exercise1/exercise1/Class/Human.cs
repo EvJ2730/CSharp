4d04d51f676ee4fd708f29b42d40d06896aedc4e
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-16 12:10:21 +0200

    new

---
+++ exercise/exercise1/exercise1/Class/Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercise1.Class
+{
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string GetPersonStats()
+        {
+            return string.Format($"Name: {FirstName}\nLast name: {LastName}\nage: {Age}");
+        }
+    }
+}
---
+++ exercise/exercise1/exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using exercise1.Class;
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human inputHuman = HumanInput();
+            static Human HumanInput()
+            {
+                Human human = new Human();
+                Console.WriteLine("Enter human's first name");
+                human.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter human's last name");
+                human.LastName = Console.ReadLine();
+                Console.WriteLine("Enter human's age");
+                human.Age = int.Parse(Console.ReadLine());
+                return human;
+            }
+            Console.WriteLine(inputHuman.GetPersonStats());
+        }
+    }
+}
