0ea893cc7ae0482dabdb35628ca6d11064eb36d4
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-04-24 16:09:31 +0200

    class08

---
+++ homework/10.04.21/class08/queue/queue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Queue myQueue = new Queue();
+            foreach(int num in myQueue)
+            {
+                
+                if(number < 0)
+                {
+                    Console.WriteLine("You have entered a wrong number");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
