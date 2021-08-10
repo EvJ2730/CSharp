fceb8c78685ce681b875c911ca6c0c36776aab01
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-08-10 20:02:26 +0200

    x

--- homework/Class03_homework/studentGroup/studentGroup/Program.cs
+++ homework/Class03_homework/studentGroup/studentGroup/Program.cs
@@ -6,16 +6,17 @@ namespace studentGroup
     {
         static void Main(string[] args)
         {
-            string[] firstGroup = new string[5] { "Angel", "Goce", "Petre", "Martin", "Nikola" };
-            string[] secondGroup = new string[5] { "Daniel", "Goran", "Bojan", "Vasil", "Dragan" };
-            Console.Write("STUDENT GROUPS -> Insert 1 or 2 to see the students in those groups: ");
+            string[] firstGroup = new string[5] { "Angela", "Eva", "Sotir", "Martin", "Jana" };
+            string[] secondGroup = new string[5] { "Daniel", "Bojana", "Lina", "Martin", "Ana" };
+            Console.Write("STUDENT GROUPS => Insert 1 to see the students in the first group." +
+                                            "Insert 2 to see the students in the second group");
         checkpoint:
             string userInput = Console.ReadLine();
             int.TryParse(userInput, out int groupNumber);
             Console.WriteLine("");
             if (groupNumber == 1)
             {
-                Console.WriteLine("Students of G1: ");
+                Console.WriteLine("Students of First Group: ");
                 foreach (var student in firstGroup)
                 {
                     Console.WriteLine(student);
@@ -23,7 +24,7 @@ namespace studentGroup
             }
             else if (groupNumber == 2)
             {
-                Console.WriteLine("Students of G2: ");
+                Console.WriteLine("Students of Second Group: ");
                 foreach (var student in secondGroup)
                 {
                     Console.WriteLine(student);
@@ -31,7 +32,7 @@ namespace studentGroup
             }
             else
             {
-                Console.Write("We only have G1 and G2 (Insert 1 or 2): ");
+                Console.Write("We only have First Group and Second Group (Insert 1 or 2): ");
                 goto checkpoint;
             }
         }
